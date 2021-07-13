ff20281f21bf1591b0c0c299bc192ccf324baa4e
Areli Flores <floresareli1998@gmail.com>
2021-07-13 16:54:35 -0600

    36

---
+++ QuieroPizza/QuieroPizza.BL/OrdenesBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuieroPizza.BL
+{
+    public class OrdenesBL
+    {
+        Contexto _contexto;
+        public List<Orden> ListadeOrdenes { get; set; }
+
+        public OrdenesBL()
+        {
+            _contexto = new Contexto();
+            ListadeOrdenes = new List<Orden>();
+        }
+
+        public List<Orden> ObtenerOrdenes()
+        {
+            ListadeOrdenes = _contexto.Ordenes
+                .Include("Cliente")
+                .ToList();
+
+            return ListadeOrdenes;
+        }
+    }
+}
---
+++ QuieroPizza/QuieroPizza.WebAdmin/Controllers/OrdenesController.cs
@@ -0,0 +1,26 @@
+﻿using QuieroPizza.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuieroPizza.WebAdmin.Controllers
+{
+    public class OrdenesController : Controller
+    {
+        OrdenesBL _ordenesBL;
+        public OrdenesController()
+        {
+            _ordenesBL = new OrdenesBL();
+        }
+
+        // GET: Ordenes
+        public ActionResult Index()
+        {
+            var listadeOrdenes = _ordenesBL.ObtenerOrdenes();
+
+            return View(listadeOrdenes);
+        }
+    }
+}
